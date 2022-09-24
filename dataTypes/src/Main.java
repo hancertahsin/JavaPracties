91305f658c57f9c79d91b679067e030fb77eb294
Tahsin <105295833+hancertahsin@users.noreply.github.com>
2022-09-24 16:21:16 +0300

    Add files via upload

---
+++ dataTypes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+   public static void main(String[] args){
+       int sayı = 12; // tam sayılar için int komutu kullanıyoruz
+       double sayı1 = 12.5; // ondalıklı sayıları tutuyoruz
+
+   }
+}
