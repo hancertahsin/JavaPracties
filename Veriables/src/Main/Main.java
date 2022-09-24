4a79d8ea4cb06d4dd6ecb03c158ea04e74c2de40
Tahsin <105295833+hancertahsin@users.noreply.github.com>
2022-09-24 16:20:20 +0300

    Add files via upload

---
+++ Veriables/src/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class Main {
+    public static void main(String[] args) {
+//öğrenci sayılarını tekrar tekrar girmemek için bu döngüyü kullanıyoruz
+// tekrar kullanılabilirlik , reusebility
+        int ogrenciSayisi = 13 ;  // int tam sayılı ifadeler
+        String mesaj = "Ögrenci sayisi: ="; // String metinsel ifade
+        System.out.println(mesaj +ogrenciSayisi);
+        System.out.println(mesaj +ogrenciSayisi);
+        System.out.println("Öğrenci sayım = "+ogrenciSayisi);
+        System.out.println("Öğrenci sayım = "+ogrenciSayisi);
+
+
+    }
+}
