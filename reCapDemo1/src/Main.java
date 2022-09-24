421b27a75e15c35acd2fdbde14175b9449888bc9
Tahsin <105295833+hancertahsin@users.noreply.github.com>
2022-09-24 16:23:11 +0300

    Add files via upload

---
+++ reCapDemo1/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int sayı1 = 20;
+        int sayı2 = 25;
+        int sayı3 = 2;
+
+        int enBuyuk = sayı1;
+        if (enBuyuk < sayı2) {
+            enBuyuk = sayı2;
+
+        }
+        if (enBuyuk < sayı3) {
+            enBuyuk = sayı3;
+
+        }
+        System.out.println("En büyük = "+ enBuyuk);
+    }
+}
