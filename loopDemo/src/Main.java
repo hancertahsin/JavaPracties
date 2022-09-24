43a12ff6797924f999b1722fc0747edff6ff5da5
Tahsin <105295833+hancertahsin@users.noreply.github.com>
2022-09-24 16:21:50 +0300

    Add files via upload

---
+++ loopDemo/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i < 10; i += 2) {
+
+            System.out.println(i);
+
+        }
+        System.out.println("For Döngüsü Bitti");
+
+        int i = 2;
+        // While Döngüsü
+
+        while (i < 10) {
+            System.out.println(i);
+            i += 2;
+        }
+        System.out.println("While Döngüsü Bitti");
+
+        //Do While döngüsü
+        int j = 100;
+        do {
+            System.out.println(j);
+            j+= 2;
+        } while (i < 10);
+        System.out.println("Do While Döngüsü Bitti");
+
+    }
+
+}
